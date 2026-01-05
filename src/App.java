013af63bdccb8d38d3865802b28b6d921a8fa87d
HenryZisow <28hzisow@gannacademy.org>
2026-01-05 11:41:44 -0500

    Done

--- src/App.java
+++ src/App.java
@@ -1,20 +1,100 @@
 import processing.core.*;
 
-public class App extends PApplet{
-    public static void main(String[] args)  {
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App extends PApplet {
+
+    Bubble firstOne;
+    ArrayList<Bubble> bubbles;
+    double timer;
+    int scene;
+    double highScore;
+    double gameStart;
+
+    public static void main(String[] args) {
         PApplet.main("App");
     }
 
-    public void setup(){
-        
+    public void setup() {
+        readHighScore();
+
+        bubbles = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            bubbleMaker();
+        }
+        scene = 0;
+        gameStart = millis();
     }
 
-    public void settings(){
-        
+    public void settings() {
+        size(1200, 800);
     }
 
-    public void draw(){
-       
+    public void draw() {
+        background(0);
 
+        if (scene == 0) {
+            for (Bubble b : bubbles) {
+                b.display();
+                b.update();
+            }
+            fill(255);
+            textSize(50);
+            timer = millis() - gameStart;
+            timer = ((int) timer / 100) / 10.0;
+            text("" + timer, width - 100, 50);
+            if (bubbles.size() == 0) {
+                scene = 1;
+                if (highScore > timer) {
+                    highScore = timer;
+                }
+            }
+        } else {
+            text("Score " + timer, 400, 400);
+            text("High score " + highScore, 400, 500);
+        }
+
+    }
+
+    public void readHighScore() {
+        try (Scanner scanner = new Scanner(Paths.get("highscore.txt"))) {
+
+            while (scanner.hasNextLine()) {
+                String row = scanner.nextLine();
+                highScore = Double.valueOf(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public void bubbleMaker() {
+        int x = (int) random(800) + 125;
+        int y = (int) random(800);
+
+        Bubble bubble = new Bubble(x, y, this);
+        bubbles.add(bubble);
+
+    }
+
+    public void keyPressed() {
+        if (key == ' ') {
+            if (scene == 0) {
+                bubbles.clear();
+            } else {
+                setup();
+            }
+        }
+    }
+
+    public void mousePressed() {
+        for (int i = 0; i < bubbles.size(); i++) {
+            Bubble b = bubbles.get(i);
+            if (b.checkTouch(mouseX, mouseY) == false) {
+                bubbles.remove(b);
+            }
+        }
     }
 }
